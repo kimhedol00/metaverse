65b0a630f258357ecd8387ead13334c0f86c5f77
kimhedol00 <hedol00@g.skku.edu>
2022-06-22 01:10:38 +0900

    Add TextRPG2

---
+++ TextRPG2/Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG2
+{
+    enum PlayerType
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Maze = 3
+    }
+    class Player : Creature
+    {
+        protected PlayerType type = PlayerType.None;
+
+
+        
+
+        protected Player(PlayerType type) : base(CreatureType.Player)
+        {
+            this.type = type;
+        }
+
+
+        
+
+        public PlayerType GetPlayerType() { return type; }
+        
+    }
+
+    class Knight : Player
+    {
+        public Knight() : base(PlayerType.Knight)
+        {
+            SetInfo(100, 10);
+        }
+    }
+
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            SetInfo(75, 12);
+        }
+    }
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Maze)
+        {
+            SetInfo(50, 15);
+        }
+    }
+
+}
