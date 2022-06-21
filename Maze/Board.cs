ff330095c056348db9e371da96112c992608590d
kimhedol00 <hedol00@g.skku.edu>
2022-06-22 00:03:00 +0900

    Add Maze.cs file

---
+++ Maze/Board.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze
+{
+    class Board
+    {
+        public TileType[,] Tile { get; private set; }
+        public int Size { get; private set; }
+        const char CIRCLE = '\u25cf';
+        public int DestY { get; private set; }
+        public int DestX { get; private set; }
+
+        Player _player;
+
+        public enum TileType
+        {
+            Empty,
+            Wall,
+        }
+
+        public void Initialize(int size, Player player)
+        {
+            if (size % 2 == 0)
+                return;
+            Tile  = new TileType[size, size];
+            Size = size;
+            _player = player;
+            DestY = Size - 2;
+            DestX = Size - 2;
+
+            //GenerateByBinaryTree();
+            GenerateBySideWinder();
+        }
+        void GenerateByBinaryTree()
+        {
+            
+
+            //Mazes for Programmers
+
+            //일단 길을 막음
+            for(int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        Tile [y, x] = TileType.Wall;
+                    else
+                        Tile [y, x] = TileType.Empty;
+                }
+            }
+
+            Random rand = new Random();
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+                    if (x == Size - 2 && y == Size - 2)
+                        continue;
+                    if (y == Size -2)
+                    {
+                        Tile [y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+                    if (x == Size - 2)
+                    {
+                        Tile [y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        Tile [y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        Tile [y + 1, x] = TileType.Empty;
+                    }
+                    
+                }
+            }
+        }
+
+        void GenerateBySideWinder()
+        {
+
+
+            //Mazes for Programmers
+
+            //일단 길을 막음
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        Tile [y, x] = TileType.Wall;
+                    else
+                        Tile [y, x] = TileType.Empty;
+                }
+            }
+
+            Random rand = new Random();
+            for (int y = 0; y < Size; y++)
+            {
+                int count = 1;
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+                    if (x == Size - 2 && y == Size - 2)
+                        continue;
+                    if (y == Size - 2)
+                    {
+                        Tile [y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+                    if (x == Size - 2)
+                    {
+                        Tile [y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        Tile [y, x + 1] = TileType.Empty;
+                        count++;
+                    }
+                    else
+                    {
+                        int randomIndex = rand.Next(0, count);
+                        Tile [y + 1, x - randomIndex * 2] = TileType.Empty;
+                        count = 1;
+                    }
+
+                }
+            }
+        }
+        public void Render()
+        {
+            ConsoleColor preColor = Console.ForegroundColor;
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    //플레이어 좌표를 가지고  와서 그 좌표와 일치하면 전용 색상으로 표시
+                    if (y == _player.PosY && x == _player.PosX)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    }
+                    else if (y == DestY && x == DestX)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    else
+                        Console.ForegroundColor = GetTileColor(Tile[y, x]);
+
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = preColor;
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch(type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+
+            }
+        }
+
+    }
+}
---
+++ Maze/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Player player = new Player();
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board);
+
+
+            Console.CursorVisible = false;
+
+
+            const int WAIT_TICK = 1000 / 30;
+            
+            int lastTick = 0;
+            while(true)
+            {
+                #region 프레임관리
+                int currentTick = System.Environment.TickCount;
+                //만약 겨
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                int deltaTick = currentTick - lastTick;
+                lastTick = currentTick;
+                #endregion
+
+
+                //Input
+
+                //Logic
+                player.Update(deltaTick);
+
+                //Rendering
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+                
+            }
+
+        }
+    }
+}
